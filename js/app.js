1842a464fbf4d4a75bb5fc319b92495276529400
AhmadAbuZeid1997 <ahmadabuzeid1997@gmail.com>
2021-01-18 22:42:37 +0200

    edit

---
+++ js/app.js
@@ -0,0 +1,129 @@
+'use strict';
+var s1=document.getElementById('b1');
+s1.addEventListener('change',do1)
+function do1(){
+if (s1.checked) {
+    
+        document.getElementById("t1").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t1").style.textDecoration = "none";
+}
+
+};
+var s2=document.getElementById('b2');
+s2.addEventListener('change',do2)
+function do2(){
+if (s2.checked) {
+    
+        document.getElementById("t2").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t2").style.textDecoration = "none";
+}
+
+}
+var s3=document.getElementById('b3');
+s3.addEventListener('change',do3)
+function do3(){
+if (s3.checked) {
+    
+        document.getElementById("t3").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t3").style.textDecoration = "none";
+}
+
+}
+var s4=document.getElementById('b4');
+s4.addEventListener('change',do4)
+function do4(){
+if (s4.checked) {
+    
+        document.getElementById("t4").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t4").style.textDecoration = "none";
+}
+
+}
+var s5=document.getElementById('b5');
+s5.addEventListener('change',do5)
+function do5(){
+if (s5.checked) {
+    
+        document.getElementById("t5").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t5").style.textDecoration = "none";
+}
+}
+var s6=document.getElementById('b6');
+s6.addEventListener('change',do6)
+function do6(){
+if (s6.checked) {
+    
+        document.getElementById("t6").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t6").style.textDecoration = "none";
+}
+
+}
+var s7=document.getElementById('b7');
+s7.addEventListener('change',do7)
+function do7(){
+if (s7.checked) {
+    
+        document.getElementById("t7").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t7").style.textDecoration = "none";
+}
+}
+var s8=document.getElementById('b8');
+s8.addEventListener('change',do8)
+function do8(){
+if (s8.checked) {
+    
+        document.getElementById("t8").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t8").style.textDecoration = "none";
+}
+
+}
+var s9=document.getElementById('b9');
+s9.addEventListener('change',do9)
+function do9(){
+if (s9.checked) {
+    
+        document.getElementById("t9").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t9").style.textDecoration = "none";
+}
+}
+var s10=document.getElementById('b10');
+s10.addEventListener('change',do10)
+function do10(){
+if (s10.checked) {
+    
+        document.getElementById("t10").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t10").style.textDecoration = "none";
+}
+}
+var s11=document.getElementById('b11');
+s11.addEventListener('change',do11)
+function do11(){
+if (s11.checked) {
+    
+        document.getElementById("t11").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t11").style.textDecoration = "none";
+}
+}
+var s12=document.getElementById('b12');
+s12.addEventListener('change',do12)
+function do12(){
+if (s12.checked) {
+    
+        document.getElementById("t12").style.textDecoration = "line-through";
+} else {
+    document.getElementById("t12").style.textDecoration = "none";
+}
+}
+
+
